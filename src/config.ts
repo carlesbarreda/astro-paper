d049060298246457ed2a7eb2f74666582fc62c6b
Carles Barreda <cbm@carlesbarreda.cat>
2023-02-18 21:19:00 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -107,6 +107,14 @@ export const PWA: Partial<VitePWAOptions> = {
   workbox: {
     additionalManifestEntries: [
       {
+        url: "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf",
+        revision: null,
+      },
+      {
+        url: "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf",
+        revision: null,
+      },
+      {
         url: "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&display=swap",
         revision: null,
       },
